9a02ecb9f77184748e174d48c662831a7401526e
Johnathan Constance <jconstance@amplify.com>
2017-07-11 14:18:59 -0400

    Correct Version Check Script
    The comment describing the if function was reversed. Also fixed some
    simple linting errors.

--- scripts/versionCheck.sh
+++ scripts/versionCheck.sh
@@ -5,11 +5,11 @@ set -e # halt script on error
 TARGET_BRANCH=$1
 IS_PULL_REQUEST=$2  # false if not a pull request,
 
-# Makes sure travis does not check version if doing a pull request
-if [ $IS_PULL_REQUEST != false ]
+# Makes sure travis checks version only if doing a pull request
+if [ "$IS_PULL_REQUEST" != "false" ]
     then
-        PACKAGE_VERSION=$(grep version package.json | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]') && echo "Package Version: $PACKAGE_VERSION"
-        CURRENT_PACKAGE_VERSION=$(git show 'origin/'"$TARGET_BRANCH"':package.json' | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]') && echo "Latest Version: $CURRENT_PACKAGE_VERSION"
+        PACKAGE_VERSION=$(grep version package.json | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d ':space:') && echo "Package Version: $PACKAGE_VERSION"
+        CURRENT_PACKAGE_VERSION=$(git show 'origin/'"$TARGET_BRANCH"':package.json' | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d ':space:') && echo "Latest Version: $CURRENT_PACKAGE_VERSION"
 
         if [ "$CURRENT_PACKAGE_VERSION" = "$PACKAGE_VERSION" ]
             then

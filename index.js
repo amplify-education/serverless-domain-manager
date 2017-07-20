93445893ecf54ad87313000ad027491cf2b6034b
Sid Nutulapati <snutulapati@amplify.com>
2017-07-20 17:12:06 -0400

    throw an error instead of return false

--- index.js
+++ index.js
@@ -235,7 +235,7 @@ class ServerlessCustomDomain {
         const endPos = hostedZoneId.length;
         return hostedZoneId.substring(startPos, endPos);
       }
-      return false;
+      throw new Error('Could not find hosted zone.');
     })
     .catch((err) => {
       throw new Error(`${err} Unable to retrieve Route53 hosted zone id.`);

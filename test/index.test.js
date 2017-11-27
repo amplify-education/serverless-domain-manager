23380882c3decf2f0a72a02e2c0cb001b0b54a1d
Malcolm Jones <majones@amplify.com>
2017-11-27 17:50:34 -0500

    Add test for migrating record types

--- test/index.test.js
+++ test/index.test.js
@@ -149,6 +149,31 @@ describe('Custom Domain Plugin', () => {
       expect(result).to.equal('foo');
     });
 
+    it('Migrate legacy CNAME records to A Alias', async () => {
+      AWS.mock('Route53', 'listHostedZones', (params, callback) => {
+        callback(null, { HostedZones: [{ Name: 'test_domain', Id: 'test_id' }] });
+      });
+      AWS.mock('Route53', 'changeResourceRecordSets', (params, callback) => {
+        callback(null, params);
+      });
+
+      const plugin = constructPlugin('test_basepath', null, true, true);
+      plugin.route53 = new aws.Route53();
+      plugin.setGivenDomainName(plugin.serverless.service.custom.customDomain.domainName);
+
+      const result = await plugin.migrateRecordType('test_distribution_name');
+      const changes = result.ChangeBatch.Changes;
+
+      expect(changes[0].Action).to.equal('DELETE');
+      expect(changes[0].ResourceRecordSet.Type).to.equal('CNAME');
+      expect(changes[0].ResourceRecordSet.Name).to.equal('test_domain');
+      expect(changes[0].ResourceRecordSet.ResourceRecords[0].Value).to.equal('test_distribution_name');
+
+      expect(changes[1].Action).to.equal('CREATE');
+      expect(changes[1].ResourceRecordSet.Type).to.equal('A');
+      expect(changes[1].ResourceRecordSet.Name).to.equal('test_domain');
+      expect(changes[1].ResourceRecordSet.AliasTarget.DNSName).to.equal('test_distribution_name');
+    });
 
     it('Create a new A Alias Record', async () => {
       AWS.mock('Route53', 'listHostedZones', (params, callback) => {

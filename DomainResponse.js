618f8da3e6922f04b9728f3fd7654cca714ddb22
Alex DeBrie <alexdebrie1@gmail.com>
2018-12-13 01:10:57 +0000

    PR comments

--- DomainResponse.js
+++ DomainResponse.js
@@ -1,10 +1,20 @@
 'use strict';
 
+// Sometimes, the getDomainName call doesn't return either a distributionHostedZoneId or a
+// regionalHostedZoneId.
+// AFAICT, this only happens with edge-optimized endpoints. The hostedZoneId for these endpoints
+// is always the one below.
+// Docs: https://docs.aws.amazon.com/general/latest/gr/rande.html#apigateway_region
+// PR: https://github.com/amplify-education/serverless-domain-manager/pull/171
+const defaultHostedZoneId = 'Z2FDTNDATAQYW2';
+
 class DomainResponse {
   constructor(data) {
     return {
       domainName: data.distributionDomainName || data.regionalDomainName,
-      hostedZoneId: data.distributionHostedZoneId || data.regionalHostedZoneId || 'Z2FDTNDATAQYW2',
+      hostedZoneId: data.distributionHostedZoneId ||
+                    data.regionalHostedZoneId ||
+                    defaultHostedZoneId,
     };
   }
 }

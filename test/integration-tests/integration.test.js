8218ba6d1e7c7284891a91c8d861bfc86db2b2cd
Sid Nutulapati <snutulapati@amplify.com>
2018-12-19 11:33:06 -0500

    uncomment code :facepalm

--- test/integration-tests/integration.test.js
+++ test/integration-tests/integration.test.js
@@ -80,52 +80,52 @@ describe('Integration Tests', function () { // eslint-disable-line func-names
     await utilities.linkPackages();
   });
 
-  // describe('Domain Manager Is Enabled', function () { // eslint-disable-line func-names
-  //   this.timeout(SIX_HOURS);
-  //
-  //   itParam('${value.testDescription}', testCases, async (value) => { // eslint-disable-line no-template-curly-in-string
-  //     const created = await utilities.createResources(value.testFolder,
-  //         value.testDomain, RANDOM_STRING, true);
-  //     if (!created) {
-  //       throw new utilities.CreationError('Resources failed to create.');
-  //     } else {
-  //       const stage = await utilities.getStage(value.testDomain);
-  //       expect(stage).to.equal(value.testStage);
-  //
-  //       const basePath = await utilities.getBasePath(value.testDomain);
-  //       expect(basePath).to.equal(value.testBasePath);
-  //
-  //       const endpoint = await utilities.getEndpointType(value.testDomain);
-  //       expect(endpoint).to.equal(value.testEndpoint);
-  //
-  //       const status = await utilities.curlUrl(value.testURL);
-  //       expect(status).to.equal(200);
-  //     }
-  //     await utilities.destroyResources(value.testFolder, value.testDomain, RANDOM_STRING);
-  //   });
-  // });
-  //
-  // describe('Domain Manager Is Not Enabled', function () { // eslint-disable-line func-names
-  //   this.timeout(5 * 60 * 1000); // 5 minutes in milliseconds
-  //   const testName = 'disabled';
-  //   const testURL = `${testName}-${RANDOM_STRING}.${TEST_DOMAIN}`;
-  //
-  //   before(async () => {
-  //     const created = await utilities.createResources(testName, testURL, RANDOM_STRING, false);
-  //     if (!created) {
-  //       throw new utilities.CreationError('Resources failed to create.');
-  //     }
-  //   });
-  //
-  //   it('Does not create a domain', async () => {
-  //     const data = await utilities.curlUrl(`https://${testURL}/hello-world`);
-  //     expect(data).to.equal(null);
-  //   });
-  //
-  //   after(async () => {
-  //     await utilities.destroyResources(testName, testURL, RANDOM_STRING);
-  //   });
-  // });
+  describe('Domain Manager Is Enabled', function () { // eslint-disable-line func-names
+    this.timeout(SIX_HOURS);
+
+    itParam('${value.testDescription}', testCases, async (value) => { // eslint-disable-line no-template-curly-in-string
+      const created = await utilities.createResources(value.testFolder,
+          value.testDomain, RANDOM_STRING, true);
+      if (!created) {
+        throw new utilities.CreationError('Resources failed to create.');
+      } else {
+        const stage = await utilities.getStage(value.testDomain);
+        expect(stage).to.equal(value.testStage);
+
+        const basePath = await utilities.getBasePath(value.testDomain);
+        expect(basePath).to.equal(value.testBasePath);
+
+        const endpoint = await utilities.getEndpointType(value.testDomain);
+        expect(endpoint).to.equal(value.testEndpoint);
+
+        const status = await utilities.curlUrl(value.testURL);
+        expect(status).to.equal(200);
+      }
+      await utilities.destroyResources(value.testFolder, value.testDomain, RANDOM_STRING);
+    });
+  });
+
+  describe('Domain Manager Is Not Enabled', function () { // eslint-disable-line func-names
+    this.timeout(5 * 60 * 1000); // 5 minutes in milliseconds
+    const testName = 'disabled';
+    const testURL = `${testName}-${RANDOM_STRING}.${TEST_DOMAIN}`;
+
+    before(async () => {
+      const created = await utilities.createResources(testName, testURL, RANDOM_STRING, false);
+      if (!created) {
+        throw new utilities.CreationError('Resources failed to create.');
+      }
+    });
+
+    it('Does not create a domain', async () => {
+      const data = await utilities.curlUrl(`https://${testURL}/hello-world`);
+      expect(data).to.equal(null);
+    });
+
+    after(async () => {
+      await utilities.destroyResources(testName, testURL, RANDOM_STRING);
+    });
+  });
 
   describe('Basepath Mapping Is Set', function () { // eslint-disable-line func-names
     this.timeout(15 * 60 * 1000); // 15 minutes in milliseconds

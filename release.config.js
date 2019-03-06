64bb33cc4ccbe8d98ece5e5f2b0699cb1b789cf3
Magic Madrigal <madrigal.ed@gmail.com>
2019-03-05 21:27:26 -0800

    CI: release

---
+++ release.config.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-useless-escape */
+/**
+ * Config file for Semantic Release
+ * @type {Object}
+ */
+module.exports = {
+  preset: 'metahub',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/github',
+    '@semantic-release/git',
+  ],
+  analyzeCommits: {
+    releaseRules: 'conventional-changelog-metahub/release-rules',
+    noteKeywords: [
+      'BREAKING CHANGE',
+      'BREAKING CHANGES',
+      'BREAKING',
+    ],
+  //  },
+  },
+  generateNotes: {
+    noteKeywords: [
+      'BREAKING CHANGE',
+      'BREAKING CHANGES',
+      'BREAKING',
+    ],
+  //  },
+  },
+};

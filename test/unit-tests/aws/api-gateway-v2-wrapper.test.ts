f9554b760d89e0faaa04482eeeb3ed12b7c792f9
rddimon <rddimon@gmail.com>
2023-07-18 15:39:23 +0300

    AT-9828: fix unit tests https://amplify-education.atlassian.net/browse/AT-9828

--- test/unit-tests/aws/api-gateway-v2-wrapper.test.ts
+++ test/unit-tests/aws/api-gateway-v2-wrapper.test.ts
@@ -341,7 +341,10 @@ describe("API Gateway V2 wrapper checks", () => {
             const commandCalls = APIGatewayMock.commandCalls(CreateApiMappingCommand, expectedParams, true);
 
             expect(commandCalls.length).to.equal(1);
-            expect(consoleOutput[0]).to.contains("V2 - Created API mapping");
+            expect(consoleOutput[0]).to.contains(
+                "Make sure the API Mapping with the `$default` stage already exists."
+            );
+            expect(consoleOutput[1]).to.contains("V2 - Created API mapping");
         });
 
         it("create base path mapping failure", async () => {

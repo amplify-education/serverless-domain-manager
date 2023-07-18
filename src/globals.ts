9877e31eecf6d665a62b7fc3d2e025318acfbbb2
rddimon <rddimon@gmail.com>
2023-07-18 21:04:07 +0300

    AT-9828: update retry strategy https://amplify-education.atlassian.net/browse/AT-9828

--- src/globals.ts
+++ src/globals.ts
@@ -75,12 +75,12 @@ export default class Globals {
         return await fromIni({profile})();
     }
 
-    public static getRetryStrategy(attempts: number = 3, delay: number = 10000) {
+    public static getRetryStrategy(attempts: number = 3, delay: number = 3000, backoff: number = 500) {
         return new ConfiguredRetryStrategy(
             attempts, // max attempts.
-            // This example sets the backoff at 100ms plus 10s per attempt.
+            // This example sets the backoff at 500ms plus 3s per attempt.
             // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/modules/_aws_sdk_util_retry.html#aws-sdkutil-retry
-            (attempt: number) => 100 + attempt * delay // backoff function.
+            (attempt: number) => backoff + attempt * delay // backoff function.
         )
     }
 }

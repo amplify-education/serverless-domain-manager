3b807aa1352f844700d835893efdceb2c8176da5
rddimon <rddimon@gmail.com>
2024-02-06 15:55:26 +0200

    AT-11105: remove unused code https://amplify-education.atlassian.net/browse/AT-11105

--- test/integration-tests/debug/debug.test.ts
+++ test/integration-tests/debug/debug.test.ts
@@ -1,7 +1,6 @@
 import "mocha";
 import utilities = require("../test-utilities");
 import { TEMP_DIR } from "../base";
-import { exec } from "../test-utilities";
 
 const CONFIGS_FOLDER = "debug";
 const TIMEOUT_MINUTES = 15 * 60 * 1000; // 15 minutes in milliseconds

12ff22be4276c52908afa757b4878fde0fb09b95
rddimon <rddimon@gmail.com>
2024-02-06 15:44:23 +0200

    AT-11105: add sleep https://amplify-education.atlassian.net/browse/AT-11105

--- src/aws/route53-wrapper.ts
+++ src/aws/route53-wrapper.ts
@@ -12,159 +12,159 @@ import {
 import { getAWSPagedResults } from "../utils";
 
 class Route53Wrapper {
-    public route53: Route53Client;
-    private readonly region: string;
+  public route53: Route53Client;
+  private readonly region: string;
 
-    constructor (credentials?: any, region?: string) {
-      // not null and not undefined
-      if (credentials) {
-        this.region = region || Globals.getRegion();
-        this.route53 = new Route53Client({
-          credentials,
-          region: this.region,
-          retryStrategy: Globals.getRetryStrategy(),
-          requestHandler: Globals.getRequestHandler()
-        });
-      } else {
-        this.region = Globals.getRegion();
-        this.route53 = new Route53Client({
-          region: this.region,
-          retryStrategy: Globals.getRetryStrategy(),
-          requestHandler: Globals.getRequestHandler()
-        });
-      }
+  constructor (credentials?: any, region?: string) {
+    // not null and not undefined
+    if (credentials) {
+      this.region = region || Globals.getRegion();
+      this.route53 = new Route53Client({
+        credentials,
+        region: this.region,
+        retryStrategy: Globals.getRetryStrategy(),
+        requestHandler: Globals.getRequestHandler()
+      });
+    } else {
+      this.region = Globals.getRegion();
+      this.route53 = new Route53Client({
+        region: this.region,
+        retryStrategy: Globals.getRetryStrategy(),
+        requestHandler: Globals.getRequestHandler()
+      });
+    }
+  }
+
+  /**
+   * Gets Route53 HostedZoneId from user or from AWS
+   */
+  public async getRoute53HostedZoneId (domain: DomainConfig, isHostedZonePrivate?: boolean): Promise<string> {
+    if (domain.hostedZoneId) {
+      Logging.logInfo(`Selected specific hostedZoneId ${domain.hostedZoneId}`);
+      return domain.hostedZoneId;
     }
 
-    /**
-     * Gets Route53 HostedZoneId from user or from AWS
-     */
-    public async getRoute53HostedZoneId (domain: DomainConfig, isHostedZonePrivate?: boolean): Promise<string> {
-      if (domain.hostedZoneId) {
-        Logging.logInfo(`Selected specific hostedZoneId ${domain.hostedZoneId}`);
-        return domain.hostedZoneId;
-      }
-
-      const isPrivateDefined = typeof isHostedZonePrivate !== "undefined";
-      if (isPrivateDefined) {
-        const zoneTypeString = isHostedZonePrivate ? "private" : "public";
-        Logging.logInfo(`Filtering to only ${zoneTypeString} zones.`);
-      }
-
-      let hostedZones = [];
-      try {
-        hostedZones = await getAWSPagedResults<HostedZone, ListHostedZonesCommandInput, ListHostedZonesCommandOutput>(
-          this.route53,
-          "HostedZones",
-          "Marker",
-          "NextMarker",
-          new ListHostedZonesCommand({})
-        );
-        Logging.logInfo(`Founded hosted zones list: ${hostedZones.map((zone) => zone.Name)}.`);
-      } catch (err) {
-        throw new Error(`Unable to list hosted zones in Route53.\n${err.message}`);
-      }
-
-      // removing the first part of the domain name, api.test.com => test.com
-      const domainNameHost = domain.givenDomainName.substring(domain.givenDomainName.indexOf(".") + 1);
-      const targetHostedZone = hostedZones
-        .filter((hostedZone) => {
-          return !isPrivateDefined || isHostedZonePrivate === hostedZone.Config.PrivateZone;
-        })
-        .filter((hostedZone) => {
-          const hostedZoneName = hostedZone.Name.replace(/\.$/, "");
-          return domain.givenDomainName === hostedZoneName || domainNameHost.endsWith(hostedZoneName);
-        })
-        .sort((zone1, zone2) => zone2.Name.length - zone1.Name.length)
-        .shift();
-
-      if (targetHostedZone) {
-        return targetHostedZone.Id.replace("/hostedzone/", "");
-      } else {
-        throw new Error(`Could not find hosted zone '${domain.givenDomainName}'`);
-      }
+    const isPrivateDefined = typeof isHostedZonePrivate !== "undefined";
+    if (isPrivateDefined) {
+      const zoneTypeString = isHostedZonePrivate ? "private" : "public";
+      Logging.logInfo(`Filtering to only ${zoneTypeString} zones.`);
     }
 
-    /**
-     * Change A Alias record through Route53 based on given action
-     * @param action: String descriptor of change to be made. Valid actions are ['UPSERT', 'DELETE']
-     * @param domain: DomainInfo object containing info about custom domain
-     */
-    public async changeResourceRecordSet (action: ChangeAction, domain: DomainConfig): Promise<void> {
-      if (domain.createRoute53Record === false) {
-        Logging.logInfo(`Skipping ${action === ChangeAction.DELETE ? "removal" : "creation"} of Route53 record.`);
-        return;
-      }
-      Logging.logInfo(`Creating/updating route53 record for '${domain.givenDomainName}'.`);
-      // Set up parameters
-      const route53HostedZoneId = await this.getRoute53HostedZoneId(domain, domain.hostedZonePrivate);
-      const route53Params = domain.route53Params;
-      const route53healthCheck = route53Params.healthCheckId ? { HealthCheckId: route53Params.healthCheckId } : {};
-      const domainInfo = domain.domainInfo ?? {
-        domainName: domain.givenDomainName,
-        hostedZoneId: route53HostedZoneId
+    let hostedZones = [];
+    try {
+      hostedZones = await getAWSPagedResults<HostedZone, ListHostedZonesCommandInput, ListHostedZonesCommandOutput>(
+        this.route53,
+        "HostedZones",
+        "Marker",
+        "NextMarker",
+        new ListHostedZonesCommand({})
+      );
+      Logging.logInfo(`Founded hosted zones list: ${hostedZones.map((zone) => zone.Name)}.`);
+    } catch (err) {
+      throw new Error(`Unable to list hosted zones in Route53.\n${err.message}`);
+    }
+
+    // removing the first part of the domain name, api.test.com => test.com
+    const domainNameHost = domain.givenDomainName.substring(domain.givenDomainName.indexOf(".") + 1);
+    const targetHostedZone = hostedZones
+      .filter((hostedZone) => {
+        return !isPrivateDefined || isHostedZonePrivate === hostedZone.Config.PrivateZone;
+      })
+      .filter((hostedZone) => {
+        const hostedZoneName = hostedZone.Name.replace(/\.$/, "");
+        return domain.givenDomainName === hostedZoneName || domainNameHost.endsWith(hostedZoneName);
+      })
+      .sort((zone1, zone2) => zone2.Name.length - zone1.Name.length)
+      .shift();
+
+    if (targetHostedZone) {
+      return targetHostedZone.Id.replace("/hostedzone/", "");
+    } else {
+      throw new Error(`Could not find hosted zone '${domain.givenDomainName}'`);
+    }
+  }
+
+  /**
+   * Change A Alias record through Route53 based on given action
+   * @param action: String descriptor of change to be made. Valid actions are ['UPSERT', 'DELETE']
+   * @param domain: DomainInfo object containing info about custom domain
+   */
+  public async changeResourceRecordSet (action: ChangeAction, domain: DomainConfig): Promise<void> {
+    if (domain.createRoute53Record === false) {
+      Logging.logInfo(`Skipping ${action === ChangeAction.DELETE ? "removal" : "creation"} of Route53 record.`);
+      return;
+    }
+    Logging.logInfo(`Creating/updating route53 record for '${domain.givenDomainName}'.`);
+    // Set up parameters
+    const route53HostedZoneId = await this.getRoute53HostedZoneId(domain, domain.hostedZonePrivate);
+    const route53Params = domain.route53Params;
+    const route53healthCheck = route53Params.healthCheckId ? { HealthCheckId: route53Params.healthCheckId } : {};
+    const domainInfo = domain.domainInfo ?? {
+      domainName: domain.givenDomainName,
+      hostedZoneId: route53HostedZoneId
+    };
+
+    let routingOptions = {};
+    if (route53Params.routingPolicy === Globals.routingPolicies.latency) {
+      routingOptions = {
+        Region: this.region,
+        SetIdentifier: route53Params.setIdentifier ?? domainInfo.domainName,
+        ...route53healthCheck
       };
+    }
 
-      let routingOptions = {};
-      if (route53Params.routingPolicy === Globals.routingPolicies.latency) {
-        routingOptions = {
-          Region: this.region,
-          SetIdentifier: route53Params.setIdentifier ?? domainInfo.domainName,
-          ...route53healthCheck
-        };
-      }
+    if (route53Params.routingPolicy === Globals.routingPolicies.weighted) {
+      routingOptions = {
+        Weight: route53Params.weight,
+        SetIdentifier: route53Params.setIdentifier ?? domainInfo.domainName,
+        ...route53healthCheck
+      };
+    }
 
-      if (route53Params.routingPolicy === Globals.routingPolicies.weighted) {
-        routingOptions = {
-          Weight: route53Params.weight,
-          SetIdentifier: route53Params.setIdentifier ?? domainInfo.domainName,
-          ...route53healthCheck
-        };
-      }
+    let hostedZoneIds: string[];
+    if (domain.splitHorizonDns) {
+      hostedZoneIds = await Promise.all([
+        this.getRoute53HostedZoneId(domain, false),
+        this.getRoute53HostedZoneId(domain, true)
+      ]);
+    } else {
+      hostedZoneIds = [route53HostedZoneId];
+    }
 
-      let hostedZoneIds: string[];
-      if (domain.splitHorizonDns) {
-        hostedZoneIds = await Promise.all([
-          this.getRoute53HostedZoneId(domain, false),
-          this.getRoute53HostedZoneId(domain, true)
-        ]);
-      } else {
-        hostedZoneIds = [route53HostedZoneId];
-      }
-
-      const recordsToCreate = domain.createRoute53IPv6Record ? [RRType.A, RRType.AAAA] : [RRType.A];
-      for (const hostedZoneId of hostedZoneIds) {
-        const changes = recordsToCreate.map((Type) => ({
-          Action: action,
-          ResourceRecordSet: {
-            AliasTarget: {
-              DNSName: domainInfo.domainName,
-              EvaluateTargetHealth: false,
-              HostedZoneId: domainInfo.hostedZoneId
-            },
-            Name: domain.givenDomainName,
-            Type,
-            ...routingOptions
-          }
-        }));
-
-        const params = {
-          ChangeBatch: {
-            Changes: changes,
-            Comment: `Record created by "${Globals.pluginName}"`
+    const recordsToCreate = domain.createRoute53IPv6Record ? [RRType.A, RRType.AAAA] : [RRType.A];
+    for (const hostedZoneId of hostedZoneIds) {
+      const changes = recordsToCreate.map((Type) => ({
+        Action: action,
+        ResourceRecordSet: {
+          AliasTarget: {
+            DNSName: domainInfo.domainName,
+            EvaluateTargetHealth: false,
+            HostedZoneId: domainInfo.hostedZoneId
           },
-          HostedZoneId: hostedZoneId
-        };
-        // Make API call
-        try {
-          await this.route53.send(new ChangeResourceRecordSetsCommand(params));
-        } catch (err) {
-          throw new Error(
-                    `Failed to ${action} ${recordsToCreate.join(",")} Alias for '${domain.givenDomainName}':\n
-                    ${err.message}`
-          );
+          Name: domain.givenDomainName,
+          Type,
+          ...routingOptions
         }
+      }));
+
+      const params = {
+        ChangeBatch: {
+          Changes: changes,
+          Comment: `Record created by "${Globals.pluginName}"`
+        },
+        HostedZoneId: hostedZoneId
+      };
+      // Make API call
+      try {
+        await this.route53.send(new ChangeResourceRecordSetsCommand(params));
+      } catch (err) {
+        throw new Error(
+          `Failed to ${action} ${recordsToCreate.join(",")} Alias for '${domain.givenDomainName}':\n
+                    ${err.message}`
+        );
       }
     }
+  }
 }
 
 export = Route53Wrapper;
--- src/aws/s3-wrapper.ts
+++ src/aws/s3-wrapper.ts
@@ -4,47 +4,47 @@ import { HeadObjectCommand, HeadObjectRequest, S3Client } from "@aws-sdk/client-
 import Globals from "../globals";
 
 class S3Wrapper {
-    public s3: S3Client;
+  public s3: S3Client;
 
-    constructor (credentials?: any) {
-      this.s3 = new S3Client({
-        credentials,
-        region: Globals.getRegion(),
-        requestHandler: Globals.getRequestHandler()
-      });
+  constructor (credentials?: any) {
+    this.s3 = new S3Client({
+      credentials,
+      region: Globals.getRegion(),
+      requestHandler: Globals.getRequestHandler()
+    });
+  }
+
+  /**
+   * * Checks whether the Mutual TLS certificate exists in S3 or not
+   */
+  public async assertTlsCertObjectExists (domain: DomainConfig): Promise<void> {
+    const { Bucket, Key } = S3Wrapper.extractBucketAndKey(domain.tlsTruststoreUri);
+    const params: HeadObjectRequest = { Bucket, Key };
+
+    if (domain.tlsTruststoreVersion) {
+      params.VersionId = domain.tlsTruststoreVersion;
     }
 
-    /**
-     * * Checks whether the Mutual TLS certificate exists in S3 or not
-     */
-    public async assertTlsCertObjectExists (domain: DomainConfig): Promise<void> {
-      const { Bucket, Key } = S3Wrapper.extractBucketAndKey(domain.tlsTruststoreUri);
-      const params: HeadObjectRequest = { Bucket, Key };
-
-      if (domain.tlsTruststoreVersion) {
-        params.VersionId = domain.tlsTruststoreVersion;
+    try {
+      await this.s3.send(new HeadObjectCommand(params));
+    } catch (err) {
+      if (!err.$metadata || err.$metadata.httpStatusCode !== 403) {
+        throw Error(`Could not head S3 object at ${domain.tlsTruststoreUri}.\n${err.message}`);
       }
 
-      try {
-        await this.s3.send(new HeadObjectCommand(params));
-      } catch (err) {
-        if (!err.$metadata || err.$metadata.httpStatusCode !== 403) {
-          throw Error(`Could not head S3 object at ${domain.tlsTruststoreUri}.\n${err.message}`);
-        }
-
-        Logging.logWarning(
-                `Forbidden to check the existence of the S3 object ${domain.tlsTruststoreUri} due to\n${err}`
-        );
-      }
+      Logging.logWarning(
+        `Forbidden to check the existence of the S3 object ${domain.tlsTruststoreUri} due to\n${err}`
+      );
     }
+  }
 
-    /**
-     * * Extracts Bucket and Key from the given s3 uri
-     */
-    private static extractBucketAndKey (uri: string): { Bucket: string; Key: string } {
-      const { hostname, pathname } = new URL(uri);
-      return { Bucket: hostname, Key: pathname.substring(1) };
-    }
+  /**
+   * * Extracts Bucket and Key from the given s3 uri
+   */
+  private static extractBucketAndKey (uri: string): { Bucket: string; Key: string } {
+    const { hostname, pathname } = new URL(uri);
+    return { Bucket: hostname, Key: pathname.substring(1) };
+  }
 }
 
 export = S3Wrapper;
